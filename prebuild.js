208dbd0a6f88be9c6029560811a3fb9c48ce790b
Tao <magicdawn@qq.com>
2024-04-16 06:04:42 +0800

    chore: add dev deps

--- prebuild.js
+++ prebuild.js
@@ -2,34 +2,34 @@
 
 const abi = require('node-abi')
 const semver = require('semver')
-const {execSync} = require('child_process')
+const { execSync } = require('child_process')
 
-const sh = cmd => {
+const sh = (cmd) => {
   console.log('[exec]: %s', cmd)
   execSync(cmd, {
     stdio: 'inherit',
   })
 }
 
-const targets = abi.supportedTargets.filter(item => {
-  const {runtime, target, lts} = item
+const targets = abi.supportedTargets.filter((item) => {
+  const { runtime, target, lts } = item
   if (runtime === 'node') {
-    return semver.gte(target, '8.0.0')
+    return semver.gte(target, '16.0.0')
   }
   if (runtime === 'electron') {
-    return semver.gte(target, '6.0.0')
+    return semver.gte(target, '20.0.0')
   }
 
   return false
 })
 
-const nodeVersions = targets.filter(item => item.runtime === 'node').map(item => item.target)
+const nodeVersions = targets.filter((item) => item.runtime === 'node').map((item) => item.target)
 const electronVersions = targets
-  .filter(item => item.runtime === 'electron')
-  .map(item => item.target)
+  .filter((item) => item.runtime === 'electron')
+  .map((item) => item.target)
 
-const nodeCmd = `yarn prebuild -r node ${nodeVersions.map(v => `-t ${v}`).join(' ')}`
-const electronCmd = `yarn prebuild -r electron ${electronVersions.map(v => `-t ${v}`).join(' ')}`
+const nodeCmd = `yarn prebuild -r node ${nodeVersions.map((v) => `-t ${v}`).join(' ')}`
+const electronCmd = `yarn prebuild -r electron ${electronVersions.map((v) => `-t ${v}`).join(' ')}`
 
 // clean
 sh(`cd ${__dirname}`)
