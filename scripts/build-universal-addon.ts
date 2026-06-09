d4882c26913580eb4e604088f2f07878ceee758e
magicdawn <magicdawn@qq.com>
2026-06-09 16:13:39 +0800

    chore: fix build script

--- scripts/build-universal-addon.ts
+++ scripts/build-universal-addon.ts
@@ -5,10 +5,10 @@ const repoRoot = path.join(import.meta.dirname, '..')
 const $ = execa({ cwd: repoRoot, stdio: 'inherit', verbose: 'short' })
 
 await $`pnpm node-gyp rebuild --arch=x64 --target_arch=x64`
-await $`mv ./build/Release/simple_mac_clipboard.node ./simple_mac_clipboard.darwin-x64.node`
+await $`cp ./build/Release/simple_mac_clipboard.node ./simple_mac_clipboard.darwin-x64.node`
 
 await $`pnpm node-gyp rebuild --arch=arm64 --target_arch=arm64`
-await $`mv ./build/Release/simple_mac_clipboard.node ./simple_mac_clipboard.darwin-arm64.node`
+await $`cp ./build/Release/simple_mac_clipboard.node ./simple_mac_clipboard.darwin-arm64.node`
 
 await $`lipo -create -output simple_mac_clipboard.node ./simple_mac_clipboard.darwin-arm64.node ./simple_mac_clipboard.darwin-x64.node`
 await $`file ./simple_mac_clipboard.node`
